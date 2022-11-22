9da43fa0c4b558f20d2b3ffac8df49ae975b022d
Diana <maritimdiane96@gmail.com>
2022-11-22 21:09:40 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide =() =>{
+    return(2000/100)
+}
+
+const square = (parameterdee) =>(parameterdee ** parameterdee);
+
+const add = (parameter1, parameter2) => (parameter1 + parameter2);
